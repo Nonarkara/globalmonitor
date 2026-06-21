bddbb6d5a5d5f7231a5ba4d05e52b9fa54fe504a
Non Arkara <nonsmartcity@gmail.com>
2026-06-21 12:09:24 +0700

    Updates

---
+++ assets/frontStatus-C7P8uPVC.js
@@ -0,0 +1,1 @@
+import{f as t}from"./index-B1TembVB.js";const f=()=>t("/api/fronts");export{f};
---
+++ assets/warConstants-DCsbyMOU.js
@@ -0,0 +1,1 @@
+const t=new Date("2026-02-28T00:00:00Z"),e=()=>Math.floor((Date.now()-t.getTime())/864e5);export{t as W,e as g};
